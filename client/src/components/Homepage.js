ad4acdf3c2e163fd3d9c3f8f26d6e027b22e0d96
tamrat-bayeh <tamrat3@gmail.com>
2020-01-16 10:51:20 +0200

    Home page reset

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import './Homepage.css';
+import Nav from 'react-bootstrap/Nav';
+import Login from './Login';
+import Signin from './Signin';
+
+
 
 
 
 class Homepage extends Component {
+    state={loginFlag:false, signInFlag:false, guestFlag:false}
     render() {
+        if (this.state.loginFlag) {
+            return   <Login/>
+        }
+        if (this.state.signInFlag) {
+            return   <Signin/>
+        }
+        if (this.state.guestFlag) {
+            return   <Signin/>
+        }
         return (
             <div>
+                    <Nav className="justify-content-center" activeKey="/home">
+                        <Nav.Item>
+                        <Nav.Link onClick={()=>this.setState({loginFlag:true})}>Login</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link onClick={()=>this.setState({signInFlag:true})}>Sign In</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link onClick={()=>this.setState({guestFlag:true})}>Guest</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
                 <div className='images'>
                 <div className='football-logo'>
                         <img src='img/serie-a-logo.png' alt="FunSport"></img>
